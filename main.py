ed72ab4e5eebc9dbad08095f61538151227302f2
arnauld <arnoldtchumno@gmail.com>
2026-04-18 19:16:03 -0400

    Initialisation de l'architecture en couches et installation des dépendances

---
+++ main.py
@@ -0,0 +1,6 @@
+import customtkinter as ctk
+import mysql.connector
+
+print("✅ customtkinter importé avec succès")
+print("✅ mysql.connector importé avec succès")
+print("✅ Tout fonctionne correctement !")
